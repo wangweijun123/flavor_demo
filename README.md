53a02a9209892834a9fbb2e7e7fa8c16504fb268
jntm <262448350@qq.com>
2024-10-30 02:34:46 +0800

    build success

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+flutter build apk --flavor free // 构建 dev flavor
+flutter build apk --flavor paid // 构建 prod flavor
--- android/app/src/main/kotlin/com/example/flavors_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flavors_demo/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.example.flavors_demo
 
+import io.flutter.BuildConfig
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+//import com.example.flavors_demo.BuildConfig
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "flavor")
+            .setMethodCallHandler { call: MethodCall?, result: MethodChannel.Result ->
+//                result.success(BuildConfig.FLAVOR)
+                result.success("FLAVOR")
+            }
+    }
+}
